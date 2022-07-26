bfb7a95982578497db574a7d68f111f9f9e24b4f
Mike Martinez <89422558+michaelmartinez2@users.noreply.github.com>
2022-07-26 15:35:04 -0500

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -3,8 +3,69 @@
 ######################################################################
 variable "region" {}
 
+variable "acct_id" {
+  type        = string
+  description = "The account numeric id"
+}
+
+variable "company_name" {
+  type    = string
+  description = "The company name"
+}
+
+variable "businessunit" {
+  type        = string
+  description = "The business unit that owns the resources"
+}
+
+variable "compliance" {
+  type        = string
+  description = "A tag value describing the compliance requirements for these resources."
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment this VPC is being deployed into (prod, dev, test, etc)"
+}
+
+variable "igwrole" {
+  type        = string
+  description = "The role for the IGW"
+  default     = "igw"
+}
+
+variable "maptag" {
+  type        = string
+  description = "The tag value for the 'map-migrated' tag."
+}
+
+variable "natgwrole" {
+  type        = string
+  description = "The role for the NAT Gateways"
+  default     = "natgw"
+}
+
+variable "rtrole" {
+  type        = string
+  description = "The role for the Route Tables"
+  default     = "rt"
+}
+
+variable "vpcrole" {
+  type        = string
+  description = "The role for the VPC"
+  default     = "vpc"
+}
+
+variable "vpceprole" {
+  type        = string
+  description = "The role for the VPC Endpoint"
+  default     = "vpcep"
+}
+
 variable "platform" {
   type    = string
+  description = "Used to help describe what is using the service"
   default = "demo"
 }
 
